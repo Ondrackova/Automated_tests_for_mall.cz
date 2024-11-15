26c8ad7097834b1c52f5fd48435e15d826a3c9b3
Zuzana Ondrackova <ondrac.zuzana@gmail.com>
2024-11-15 12:21:03 +0100

    Create changes in tests, fixes in tests - DeliveryMalltest, ComplaintsTests, CoffeeTest

--- mall.cz/src/test/java/HomePageTest.java
+++ mall.cz/src/test/java/HomePageTest.java
@@ -13,59 +13,58 @@ public class HomePageTest {
 
     @Test
     void homePageTest() {
+        //open browser
         WebDriver browser = WebDriverManager.firefoxdriver().create();
-        browser.get("https://mall.cz");
-         //accept cookies
-        WebElement cookiesAcceptButton = browser.findElement(By.cssSelector(".legal-consent__button-container"));
-        cookiesAcceptButton.click();
-
-        Assertions.assertEquals("MALL.CZ – bílé zboží, elektronika, PC, outdoor, hobby, hračky, kosmetika, chovatelské potřeby", browser.getTitle());
-        //later click a button on its index (0,1,2)
-       // browser.findElements((By.cssSelector("legal-consent__button-container")).get(2).click());
-    }
-
-    @Test
-    void HairDryerTest () {
-        WebDriver browser = WebDriverManager.firefoxdriver().create();
-        WebDriverWait browserWait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        //loading the web
         browser.get("https://mall.cz");
         //accept cookies
         WebElement cookiesAcceptButton = browser.findElement(By.cssSelector(".legal-consent__button-container"));
         cookiesAcceptButton.click();
 
-        //click on Spotrebice
+        //assert
+        Assertions.assertEquals("MALL.CZ – bílé zboží, elektronika, PC, outdoor, hobby, hračky, kosmetika, chovatelské potřeby", browser.getTitle());
+        //later click a button on its index (0,1,2)
+        //browser.findElements((By.cssSelector("legal-consent__button-container")).get(2).click());
+    }
+
+    @Test
+    void HairDryerTest () {
+        //open browser
+        WebDriver browser = WebDriverManager.firefoxdriver().create();
+        //add time for waiting 10 s
+        WebDriverWait browserWait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        //loading the web
+        browser.get("https://mall.cz");
+        //accept cookies
+        WebElement cookiesAcceptButton = browser.findElement(By.cssSelector(".legal-consent__button-container"));
+        cookiesAcceptButton.click();
+        //click on Appliances
         browser.findElement(By.cssSelector(".desktop-menu__item-title")).click();
-
-        //click on hair dryers / feny
+        //click on hair dryers /feny
         browserWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/feny']"))).click();
-       // browser.findElement(By.xpath("//a[@href='/feny']")).click();
-
         //click on fen Philips
         browserWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".bs__title"))).click();
-        //browser.findElement(By.cssSelector(".bs__title")).click();
-
         //check item
         var expectedName = browserWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".detail__title--desktop"))).getText();
-        //var expectedName = browser.findElement(By.cssSelector(".detail__title--desktop")).getText();
 
         //add to cart
         browser.findElement(By.cssSelector(".info-box__main-btn .add-to-cart-list")).click();
-
         //open cart
         browserWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".cross-sell__button__to-cart__to"))).click();
-        //browser.findElement(By.cssSelector(".cross-sell__button__to-cart__to")).click();
-
-        //check item in cart
+        //check items in cart
         var actualName = browserWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".cart-overview-item-row__title-and-params"))).getText();     //var actualName = browser.findElement(By.cssSelector(".cart-overview-item-row__title-and-params")).getText();
 
-        //if in cart is the same item as we want
+        //check counts of items
         Assertions.assertEquals(expectedName, actualName);
     }
 
     @Test
-    void PricesMallTest () {
+    void DeliveryMallTest () {
+        //open browser
         WebDriver browser = WebDriverManager.firefoxdriver().create();
+        //add time for waiting 10 s
         WebDriverWait browserWait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        //loading the web
         browser.get("https://mall.cz");
         //accept cookie
         WebElement cookiesAcceptButton = browser.findElement(By.cssSelector(".legal-consent__button-container"));
@@ -73,11 +72,10 @@ public class HomePageTest {
 
         //prices and delivery
         browserWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".list-item__link__text"))).click();
-        //browser.findElement(By.cssSelector(".list-item__link__text")).click();
-
         //check partner delivery
-        //WebElement button = browser.findElement(By.cssSelector(".osobni .cnt"));
         WebElement button = browserWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".osobni .cnt")));
+
+        //assert present partner delivery
         Assertions.assertTrue(button.isDisplayed());
     }
     @Test
